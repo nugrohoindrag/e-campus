2226ea72fc8ecf33aa6b6939148e3e67e9a973d2
nugrohoindrag <nugrohoindrag@gmail.com>
2019-11-10 15:09:59 +0700

    commit

--- resources/views/siswa/profile.blade.php
+++ resources/views/siswa/profile.blade.php
@@ -37,10 +37,9 @@
                                 <div class="profile-info">
                                     <h4 class="heading">Basic Info</h4>
                                     <ul class="list-unstyled list-justify">
-                                        <li>Birthdate <span>24 Aug, 2016</span></li>
-                                        <li>Mobile <span>(124) 823409234</span></li>
-                                        <li>Email <span>samuel@mydomain.com</span></li>
-                                        <li>Website <span><a href="https://www.themeineed.com">www.themeineed.com</a></span></li>
+                                        <li>Gender <span>{{$siswa->gender}}</span></li>
+                                        <li>Religion <span>{{$siswa->religion}}</span></li>
+                                        <li>Address <span>{{$siswa->address}}</span></li>
                                     </ul>
                                 </div>
                                 <div class="text-center"><a href="#" class="btn btn-primary">Edit Profile</a></div>
