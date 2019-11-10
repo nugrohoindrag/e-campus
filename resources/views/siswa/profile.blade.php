020134576d748c2504403bb0e6914f5cf38f1a9b
nugrohoindrag <nugrohoindrag@gmail.com>
2019-11-10 11:48:04 +0700

    commit

--- resources/views/siswa/profile.blade.php
+++ resources/views/siswa/profile.blade.php
@@ -43,10 +43,6 @@
                                         <li>Website <span><a href="https://www.themeineed.com">www.themeineed.com</a></span></li>
                                     </ul>
                                 </div>
-                                <div class="profile-info">
-                                    <h4 class="heading">About</h4>
-                                    <p>Interactively fashion excellent information after distinctive outsourcing.</p>
-                                </div>
                                 <div class="text-center"><a href="#" class="btn btn-primary">Edit Profile</a></div>
                             </div>
                             <!-- END PROFILE DETAIL -->
@@ -54,47 +50,6 @@
                         <!-- END LEFT COLUMN -->
                         <!-- RIGHT COLUMN -->
                         <div class="profile-right">
-                            <h4 class="heading">Samuel's Awards</h4>
-                            <!-- AWARDS -->
-                            <div class="awards">
-                                <div class="row">
-                                    <div class="col-md-3 col-sm-6">
-                                        <div class="award-item">
-                                            <div class="hexagon">
-                                                <span class="lnr lnr-sun award-icon"></span>
-                                            </div>
-                                            <span>Most Bright Idea</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3 col-sm-6">
-                                        <div class="award-item">
-                                            <div class="hexagon">
-                                                <span class="lnr lnr-clock award-icon"></span>
-                                            </div>
-                                            <span>Most On-Time</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3 col-sm-6">
-                                        <div class="award-item">
-                                            <div class="hexagon">
-                                                <span class="lnr lnr-magic-wand award-icon"></span>
-                                            </div>
-                                            <span>Problem Solver</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3 col-sm-6">
-                                        <div class="award-item">
-                                            <div class="hexagon">
-                                                <span class="lnr lnr-heart award-icon"></span>
-                                            </div>
-                                            <span>Most Loved</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="text-center"><a href="#" class="btn btn-default">See all awards</a></div>
-                            </div>
-                            <!-- END AWARDS -->
-                            <!-- TABBED CONTENT -->
                             <div class="custom-tabs-line tabs-line-bottom left-aligned">
                                 <ul class="nav" role="tablist">
                                     <li class="active"><a href="#tab-bottom-left1" role="tab" data-toggle="tab">Recent Activity</a></li>
@@ -122,96 +77,6 @@
                                     </ul>
                                     <div class="margin-top-30 text-center"><a href="#" class="btn btn-default">See all activity</a></div>
                                 </div>
-                                <div class="tab-pane fade" id="tab-bottom-left2">
-                                    <div class="table-responsive">
-                                        <table class="table project-table">
-                                            <thead>
-                                                <tr>
-                                                    <th>Title</th>
-                                                    <th>Progress</th>
-                                                    <th>Leader</th>
-                                                    <th>Status</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td><a href="#">Spot Media</a></td>
-                                                    <td>
-                                                        <div class="progress">
-                                                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                                                                <span>60% Complete</span>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td><img src="assets/img/user2.png" alt="Avatar" class="avatar img-circle"> <a href="#">Michael</a></td>
-                                                    <td><span class="label label-success">ACTIVE</span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><a href="#">E-Commerce Site</a></td>
-                                                    <td>
-                                                        <div class="progress">
-                                                            <div class="progress-bar" role="progressbar" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100" style="width: 33%;">
-                                                                <span>33% Complete</span>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-                                                    <td><span class="label label-warning">PENDING</span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><a href="#">Project 123GO</a></td>
-                                                    <td>
-                                                        <div class="progress">
-                                                            <div class="progress-bar" role="progressbar" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100" style="width: 68%;">
-                                                                <span>68% Complete</span>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-                                                    <td><span class="label label-success">ACTIVE</span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><a href="#">Wordpress Theme</a></td>
-                                                    <td>
-                                                        <div class="progress">
-                                                            <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
-                                                                <span>75%</span>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td><img src="assets/img/user2.png" alt="Avatar" class="avatar img-circle"> <a href="#">Michael</a></td>
-                                                    <td><span class="label label-success">ACTIVE</span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><a href="#">Project 123GO</a></td>
-                                                    <td>
-                                                        <div class="progress">
-                                                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-                                                                <span>100%</span>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-                                                    <td><span class="label label-default">CLOSED</span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td><a href="#">Redesign Landing Page</a></td>
-                                                    <td>
-                                                        <div class="progress">
-                                                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-                                                                <span>100%</span>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td><img src="assets/img/user5.png" alt="Avatar" class="avatar img-circle"> <a href="#">Jason</a></td>
-                                                    <td><span class="label label-default">CLOSED</span></td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- END TABBED CONTENT -->
                         </div>
                         <!-- END RIGHT COLUMN -->
                     </div>
